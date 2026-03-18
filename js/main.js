2acc62d048626352ebdf196c420a42197fef2049
nic x <147455788+nicholasxdavis@users.noreply.github.com>
2026-03-18 11:16:04 -0600

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * Main Application Entry Point
- * This file is kept for future Vue.js integration
- * Currently, initialization happens in init.js
- */
-
-// All initialization is handled in init.js
-// This file is kept for future Vue.js integration
